36a8b35baca846c67d277f6aa6dbfde4526b4d4d
PavelSapegin <pawsapegin@yandex.ru>
2026-05-08 22:57:02 +0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,12 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(compose.desktop.currentOs)
     implementation(compose.material3)
+    implementation("org.jetbrains.exposed:exposed-core:0.50.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.50.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.50.0")
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.50.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks.test {
